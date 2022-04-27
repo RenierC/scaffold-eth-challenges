87500c42bf6b13cba0c93e925c4ad20f9d2c555f
RenierC <reniercuervo@gmail.com>
2022-04-26 23:50:58 -0400

    adds solution for second challenge

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -5,19 +5,72 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
+    uint256 public constant tokensPerEth = 100;
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event Withdrawal(address owner, uint256 amount);
+    event SellTokens(
+        address buyer,
+        uint256 amountOfETH,
+        uint256 amountOfTokens
+    );
 
-  YourToken public yourToken;
+    YourToken public yourToken;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        require(msg.value > 0, "You must send a non zero amount of ETH");
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+        uint256 amountToSend = msg.value * tokensPerEth;
 
-  // ToDo: create a sellTokens() function:
+        require(
+            yourToken.balanceOf(address(this)) >= amountToSend,
+            "The contract doesn't have enough tokens to cover the amount requested"
+        );
 
+        yourToken.transfer(msg.sender, msg.value * tokensPerEth);
+
+        emit BuyTokens(msg.sender, msg.value, amountToSend);
+    }
+
+    // FIXME: it's part of the challenge but the owner shouldn't be able to withdraw the contract's funds
+    function withdraw() public onlyOwner {
+        uint256 contractBalance = address(this).balance;
+        require(contractBalance > 0, "You don't have any tokens to withdraw");
+
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "The call failed");
+
+        emit Withdrawal(msg.sender, contractBalance);
+    }
+
+    function sellTokens(uint256 amountToSell) public payable {
+        require(amountToSell > 0, "You must send a non zero amount of tokens");
+        require(
+            yourToken.balanceOf(msg.sender) >= amountToSell,
+            "You don't have enough tokens to sell"
+        );
+
+        uint256 amountToSend = amountToSell / tokensPerEth;
+
+        require(
+            address(this).balance >= amountToSend,
+            "The contract doesn't have enough ETH to cover the amount requested"
+        );
+
+        bool charge = yourToken.transferFrom(
+            msg.sender,
+            address(this),
+            amountToSell
+        );
+        require(charge, "Failed to get user's tokens");
+
+        (bool success, ) = msg.sender.call{value: amountToSend}("");
+        require(success, "Failed to send ETH to the user");
+
+        emit SellTokens(msg.sender, amountToSell, amountToSend);
+    }
 }
